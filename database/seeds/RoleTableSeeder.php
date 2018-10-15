9dfc64f914d8013d567cfcca5a2db6da44e97021
SFFan123 <jack.o.neill.fabi@t-online.de>
2018-10-15 19:04:22 +0200

    Filled Role Seeder
    Added logic to Role seeder
    Added call of Role seeder to DatabaseSeeder

--- database/seeds/RoleTableSeeder.php
+++ database/seeds/RoleTableSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Role;
 
 class RoleTableSeeder extends Seeder
 {
@@ -11,6 +12,14 @@ class RoleTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $role_employee = new Role();
+        $role_employee->name = 'user';
+        $role_employee->description = 'A Normal User';
+        $role_employee->save();
+
+        $role_manager = new Role();
+        $role_manager->name = 'manager';
+        $role_manager->description = 'A Manager User';
+        $role_manager->save();
     }
 }
